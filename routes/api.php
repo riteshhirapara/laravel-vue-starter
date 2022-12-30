9154d055ae30235abbb195711aa123608ae4126d
Ritesh Hirapara <ritzhirapara@gmail.com>
2022-12-30 18:07:13 +0530

    Initial commit

---
+++ routes/api.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('version', function () {
+    return response()->json(['version' => config('app.version')]);
+});
+
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    Log::debug('User:' . serialize($request->user()));
+    return $request->user();
+});
+
+
+Route::namespace('App\\Http\\Controllers\\API\V1')->group(function () {
+    Route::get('profile', 'ProfileController@profile');
+    Route::put('profile', 'ProfileController@updateProfile');
+    Route::post('change-password', 'ProfileController@changePassword');
+    Route::get('tag/list', 'TagController@list');
+    Route::get('category/list', 'CategoryController@list');
+    Route::post('product/upload', 'ProductController@upload');
+
+    Route::apiResources([
+        'user' => 'UserController',
+        'product' => 'ProductController',
+        'category' => 'CategoryController',
+        'tag' => 'TagController',
+    ]);
+});
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes(['verify' => true]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('home', function () {
+    return redirect('/dashboard');
+});
+
+Route::get('/{vue_capture?}', function () {
+    return view('home');
+})->where('vue_capture', '[\/\w\.-]*')->middleware('auth');
